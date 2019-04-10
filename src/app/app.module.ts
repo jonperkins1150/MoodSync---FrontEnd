bf37d1acf7d03eda85279edd94bd032af2ccbdf8
VividerAphid <trever.kress@gmail.com>
2019-04-10 10:22:44 -0400

    Created music component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeLoggedComponent } from './home-logged/home-logged.component';
 import { HomeNotLoggedComponent } from './home-not-logged/home-not-logged.component';
+import { MusicComponent } from './music/music.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeLoggedComponent,
-    HomeNotLoggedComponent
+    HomeNotLoggedComponent,
+    MusicComponent
   ],
   imports: [
     BrowserModule,
