d6a14c20ad4d2ebe871824589f421ce5d1d15fe7
VividerAphid <trever.kress@gmail.com>
2019-04-10 10:17:03 -0400

    Created homeLogged component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeLoggedComponent } from './home-logged/home-logged.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeLoggedComponent
   ],
   imports: [
     BrowserModule,
