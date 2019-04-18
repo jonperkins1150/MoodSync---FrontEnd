94cdc5a0dd16f2c88261ef7d6101293eb1b9c956
lisajkilgore <lisajkilgore@gmail.com>
2019-04-18 14:06:01 -0400

    updated package.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,3 +10,16 @@ switch(window.location.hostname){
 export const environment = {
   production: true
 };
+
+export let APIURL = '';
+
+switch (window.location.hostname) {
+    // this is the deployed angular application
+    case 'kcp-pieclient.herokuapp.com':
+        // this is the full url of your deployed API
+        APIURL = 'https://kcp-pieapi.herokuapp.com'
+        break;
+    default:
+        // this is the local host name of your API
+        APIURL = 'http://localhost:<Port of your local API>';
+}
