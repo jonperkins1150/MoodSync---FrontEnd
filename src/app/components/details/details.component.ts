b0c14a320a87fc5013436ec8e6c3b6b023e6761d
VividerAphid <trever.kress@gmail.com>
2019-04-22 11:23:17 -0400

    Added ts to generate a form element for each property we need

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { SongService } from 'src/app/services/song.service';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
@@ -11,17 +12,42 @@ export class DetailsComponent implements OnInit {
   data : {}
   tst = {asdf: "ye",
         fdsa: "ya",
-        qwer: "yoi"
+        qwer: "yoi",
+        rewq: "yuh"
   };
-  constructor() { }
+  public dataForm: FormGroup;
+  constructor(private _form: FormBuilder) { }
 
   ngOnInit() {
     //this.dtype = "genre";
-    console.log("data: " + this.dtype);
-    
+    //console.log("data: " + this.dtype);
+    this.makeElements(this.tst);
+    var yep = this.doThingiesAndStuff(this.tst);
+    this.dataForm = this._form.group(yep);
+    console.log(this._form);
+    console.log(this.dataForm.value);
   }
 
-  doThingiesAndStuff(){
-    
+  doThingiesAndStuff(ent){
+    var formTemplate = {};
+    for(let r in ent){
+      formTemplate[r] = new FormControl;
+      formTemplate[r].value = "yuh"
+    }
+    return formTemplate;
+  }
+
+  makeElements(ent){
+    for(let r in ent){
+      this.addElement(r + "lbl", "label", document.getElementById("angForm"), r);
+      this.addElement(r, "input", document.getElementById("angForm"), "");
+    }
+  }
+
+  addElement(id, type, parent, innards){
+    var child = document.createElement(type);
+    child.id = id;
+    parent.appendChild(child);
+    child.innerHTML = innards || "";
   }
 }
