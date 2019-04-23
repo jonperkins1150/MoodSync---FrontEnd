23fa7cb02b8d21d7df4d10de81d1236ec583ad29
VividerAphid <trever.kress@gmail.com>
2019-04-23 11:51:55 -0400

    Basic services set up, testing and views in progress

--- src/app/components/song/song.component.ts
+++ src/app/components/song/song.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { SongService } from 'src/app/services/song.service';
+import { Router } from '@angular/router';
+import { SongDetails } from 'src/app/models/SongDetails';
+import { CreateSong } from 'src/app/models/CreateSong';
 
 @Component({
   selector: 'app-song',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./song.component.css']
 })
 export class SongComponent implements OnInit {
+  songs : {}
+  tstDat: CreateSong =
+  {SongId: 1,
+   SongName: "garba",
+   Artist: "Yoil",
+   Album: "The Great Garbadarpe",
+   GenreId: 9,
+   ChildFriendly: false
+  }
 
-  constructor() { }
+  constructor(private songserv: SongService,  private _router: Router) { }
 
   ngOnInit() {
+    console.log(this.songserv.createSong(this.tstDat));
+    this.songserv.getSong("all", "").subscribe(data => {
+      this.songs = data;
+      console.log(this.songs);
+    });
+  }
+  onSubmit(){
+    this.songserv.createSong(this.tstDat).subscribe(data => {
+      this._router.navigate(['/song']);
+    })
   }
 
 }
---
+++ src/app/models/CreateSong.ts
@@ -0,0 +1,8 @@
+export interface CreateSong{
+    SongId: number;
+    SongName: string;
+    Artist: string;
+    Album: string;
+    GenreId: number;
+    ChildFriendly: boolean;
+}
