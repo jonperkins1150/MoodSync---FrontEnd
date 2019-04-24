992c04ab3fbe0804c091c4887f49450533fe79ce
jonperkins1150@gmail.com <jonperkins1150@gmail.com>
2019-04-23 21:36:24 -0400

    added mood edit/delete

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,16 +1,15 @@
-    
 export let Api_Url = '';
 switch(window.location.hostname){
-  case 'https//localhost:4200':
-  Api_Url += 'http://localhost:49729/';
-    break;
-    case 'https//localhost:4200':
-    Api_Url += 'http://localhost:49729/';
-    break;
-  default:
-    Api_Url += 'http://localhost:49729/';
-    break;
+ case 'https//localhost:4200':
+ Api_Url += 'http://localhost:49729/';
+   break;
+ case 'https://moodsync1150.herokuapp.com/':
+   Api_Url = 'https://moodsync.azurewebsites.net/';
+   break;
+ default:
+   Api_Url += 'https://moodsync.azurewebsites.net/';
+   break;
 }
 export const environment = {
-  production: true
+ production: true
 };
