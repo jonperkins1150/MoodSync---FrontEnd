41da40e8a6541125c8892edce80e3c760f9ac12c
VividerAphid <trever.kress@gmail.com>
2019-04-24 01:29:22 -0400

    More tweaks

--- src/app/components/song/song-edit/song-edit.component.ts
+++ src/app/components/song/song-edit/song-edit.component.ts
@@ -11,27 +11,27 @@ import { SongDetails } from 'src/app/models/SongDetails';
 })
 
 export class SongEditComponent implements OnInit {
-  
+
   song: SongDetails;
 
   songForm: FormGroup;
 
   constructor(private _form: FormBuilder,
-              private songserv: SongService,
-              private _ar: ActivatedRoute,
-              private _router: Router) { 
+    private songserv: SongService,
+    private _ar: ActivatedRoute,
+    private _router: Router) {
 
-this._ar.paramMap.subscribe(p => {
-  this.songserv.getSong("id", p.get('id')).subscribe((singleSong: SongDetails) => {
-    this.song = singleSong;
-    this.createForm();
-  });
-});  
-}
+    this._ar.paramMap.subscribe(p => {
+      this.songserv.getSong("id", p.get('id')).subscribe((singleSong: SongDetails) => {
+        this.song = singleSong;
+        this.createForm();
+      });
+    });
+  }
 
   ngOnInit() {
   }
-  createForm(){
+  createForm() {
     this.songForm = this._form.group({
       SongName: new FormControl,
       Album: new FormControl,
@@ -40,16 +40,16 @@ this._ar.paramMap.subscribe(p => {
       ChildFriendly: new FormControl
     });
   }
-onSubmit(songForm) {
-  const updateSong: SongDetails = {
-    SongName: songForm.value.SongId,
-    Album: songForm.value.Album,
-    Artist: songForm.value.Artist,
-    GenreId: songForm.value.GenreId,
-    ChildFriendly: songForm.value.ChildFriendly  
-  };
-  this.songserv.updateSong(updateSong).subscribe(d => {
-    this._router.navigate(['/song']);
-  });
-}
+  onSubmit(songForm) {
+    const updateSong: SongDetails = {
+      SongName: songForm.value.SongId,
+      Album: songForm.value.Album,
+      Artist: songForm.value.Artist,
+      GenreId: songForm.value.GenreId,
+      ChildFriendly: songForm.value.ChildFriendly
+    };
+    this.songserv.updateSong(updateSong).subscribe(d => {
+      this._router.navigate(['/song']);
+    });
+  }
 }
